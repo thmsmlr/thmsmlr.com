f4867d32673ba8e5fd34e828939f1dd7f0d489b2
Thomas <tmillar@stitchfix.com>
2019-11-10 19:48:05 -0800

    Add Google Analytics tracking

--- layouts/index.js
+++ layouts/index.js
@@ -6,6 +6,17 @@ export default ({ children }) => {
       <Head>
         <link rel="shortcut icon" href="/images/icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-30455574-1"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-30455574-1');
+          `}
+        </script>
       </Head>
       {children}
       <style jsx global>
