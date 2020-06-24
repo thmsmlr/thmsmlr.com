5cbd2579e880c12b3f3510066434b2a54a7d61b6
Thomas <tmillar@stitchfix.com>
2020-06-23 23:52:19 -0700

    Use tailwind and notion to power the blog

--- layouts/index.js
+++ layouts/index.js
@@ -19,32 +19,6 @@ export default ({ children }) => {
         />
       </Head>
       {children}
-      <style jsx global>
-        {`
-          * {
-            box-sizing: border-box;
-          }
-
-          html,
-          body {
-            font: 100%/1.75 'Merriweather', 'Georgia', serif;
-            font-weight: 400;
-            word-wrap: break-word;
-            font-kerning: normal;
-            font-feature-settings: 'kern', 'liga', 'clig', 'calt';
-            color: hsla(0, 0%, 0%, 0.9);
-          }
-
-          p {
-            margin: 1.5rem 0 0 0;
-          }
-
-          img {
-            max-width: 100%;
-            max-height: 100%;
-          }
-        `}
-      </style>
     </>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.export = {
+  variants: {
+    border: ['responsive', 'hover']
+  }
+};
