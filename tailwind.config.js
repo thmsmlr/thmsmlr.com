682d1a08da1a3e358210d1cec4beab8db290fea1
thmsmlr <millar.thomas@gmail.com>
2020-09-26 13:53:13 -0700

    fix tailwind config to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
-module.export = {
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
   variants: {
     border: ['responsive', 'hover']
   }
-};
+  plugins: [],
+}
